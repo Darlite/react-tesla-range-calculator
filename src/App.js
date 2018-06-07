938c4891bdbea05da893380cf26836628ba35865
Ihar_Chumakou <ihar_chumakou@epam.com>
2018-06-07 15:46:36 +0300

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Header from './components/Header/Header';
+
 class App extends Component {
   render() {
     return (
       <div>
+        <Header/>
         <h2>Let's get started</h2>
       </div>
     );
