f6faf8103025b44397eb19d732b9d9e65ac110c2
Ihar_Chumakou <ihar_chumakou@epam.com>
2018-06-07 17:26:09 +0300

    Created TeslaNotice component

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './TeslaBattery.css';
 
+import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
+
 class TeslaBattery extends Component {
   render() {
     return (
       <form className='tesla-battery'>
         <h1>Range per charge</h1>
+        <TeslaNotice />
       </form>
     )
   }
