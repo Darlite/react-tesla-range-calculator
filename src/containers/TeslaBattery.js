338e3636a64f7627b3ec33d45eb664c0f74469f4
Ihar_Chumakou <ihar_chumakou@epam.com>
2018-07-19 15:49:57 +0300

    Added TeslaCar component

--- src/containers/TeslaBattery.js
+++ src/containers/TeslaBattery.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './TeslaBattery.css';
 
+import TeslaCar from '../components/TeslaCar/TeslaCar';
 import TeslaNotice from '../components/TeslaNotice/TeslaNotice';
 
 class TeslaBattery extends Component {
@@ -8,6 +9,7 @@ class TeslaBattery extends Component {
     return (
       <form className='tesla-battery'>
         <h1>Range per charge</h1>
+        <TeslaCar />
         <TeslaNotice />
       </form>
     )
